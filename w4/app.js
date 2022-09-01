990d7f4b9d1baba23ff7b57e7977fcc47d94725b
bmcgauley <brian@mcgauley.com>
2022-09-01 14:44:36 -0700

    w4 updated

--- w4/app.js
+++ w4/app.js
@@ -5,12 +5,9 @@ const showOnPage = function(text) {
     let outputDiv = document.getElementById('output')
     outputDiv.append(newParagraph)
 }
-    
-     
-     
 
 let decision = {
-    time: 0,
+    time: 1400,
     number: 1,
     updates: true,
     message: 'test',
@@ -29,7 +26,17 @@ let decision = {
         }
     },
 
-    
+    changeText: function (first, second, third) {
+        let result1 = parseFloat(document.getElementById("first").value)
+        let result2 = parseFloat(document.getElementById("second").value)
+        let result3 = document.getElementById("third").value
+        decision.isAppropriate(result1, result2, result3)
+        decision.time = result1
+        decision.number = result2
+        decision.updates = result3
+        
+        
+    },
 
     clearText: function () {
         document.getElementById("first").value=""
@@ -42,24 +49,6 @@ let decision = {
     
 
 }
-    
-let changeText = function (first, second, third) {
-    let result1 = parseFloat(document.getElementById("first").value)
-    let result2 = parseFloat(document.getElementById("second").value)
-    let result3 = document.getElementById("third").value
-    decision.isAppropriate(result1, result2, result3)
-    decision.time = result1
-    decision.number = result2
-    decision.updates = result3
-    
-    
-}
-
-
-
-
 decision.isAppropriate(decision.time, decision.number, decision.updates)
-
 showOnPage(`<h1>List of variables that influence method</h1><p>time (time of day) = ${decision.time}<p>number (number of times I've already called this week) = ${decision.number}<p>updates (if I have major important updates) = ${decision.updates}<p><----------------------------><p>${decision.message}`)
-
 showOnPage(decision.time)
