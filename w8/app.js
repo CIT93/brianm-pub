327f7f2f72439b1b6d0d87b5eae9de808ccdcc35
bmcgauley <brian@mcgauley.com>
2022-09-29 14:31:50 -0700

    w8

--- w8/app.js
+++ w8/app.js
@@ -1,107 +1,57 @@
-const daily = [
-	{
-		day: 'monday',
-		time: 1400,
-		number: 5,
-		updates: true,
-		message: '',
-	},
-];
+const daily = [{}];
 
-const filters = {
-	day: 'monday',
-	time: 1400,
-	number: 5,
-	updates: true,
-	message: '',
+const filters = {};
+
+const summaryTitle = document.createElement('h1');
+summaryTitle.textContent = 'Should You Call Your Mom?';
+document.querySelector('#header').appendChild(summaryTitle);
+
+const updateRender = function (filters) {
+	console.log(daily);
+	let outputMessage = document.createElement('p');
+	outputMessage.innerHTML = filters.message;
+	document.querySelector('#output').appendChild(outputMessage);
 };
-
-const renderOutput = function (filters) {
-	let time = filters.time;
-	let number = filters.number;
-	let updates = filters.updates;
-
-	if (time >= 1200 && time <= 1600 && number < 3 && updates) {
-		filters.message =
-			'<h2>It is a perfect time and situation to call mom!</h1><p>Call her now!</h2>';
-	} else {
-		filters.message =
-			'<h1>It is not a good time or situation to call mom</h1><p>Try again tomorrow</h2>';
-	}
-	
-};
-const processDaily = function (obj) {
-	daily.forEach(function () {
-		renderOutput(obj);
-	});
-};
-const loopOverDaily = function () {
-	daily.forEach(function (obj) {
-		processDaily(obj);
-	});
-};
-
-loopOverDaily(renderOutput(filters));
-
-document.querySelector('#day-input').addEventListener('input', function (e) {
-	console.log(e.target.value);
-	filters.day = e.target.value;
-});
-
-document
-	.querySelector('#time-military')
-	.addEventListener('input', function (e) {
-		console.log(e.target.value);
-		filters.time = Number.parseFloat(e.target.value);
-	});
-
-document
-	.querySelector('#number-of-times')
-	.addEventListener('input', function (e) {
-		console.log(e.target.value);
-		filters.number = e.target.value;
-	});
-
-document.querySelector('#updates').addEventListener('change', function (e) {
-	console.log(e.target.checked);
-	filters.updates = e.target.checked;
-});
-
-document
-	.querySelector('#submit-button')
-	.addEventListener('click', function (e) {
-		e.target.textContent = 'Submitted!';
-		renderOutput(filters);
-	});
 
 document
 	.querySelector('#user-input-form')
 	.addEventListener('submit', function (e) {
 		e.preventDefault();
-		daily.push(filters);
-		console.log(filters);
 
-		updateRender(filters);
+		filters.day = e.target.querySelector('#day-input').value;
+		filters.time = e.target.querySelector('#time-military').value;
+		filters.number = e.target.querySelector('#number-of-times').value;
+		filters.updates = e.target.querySelector('#updates').checked;
+
+		if (
+			filters.time >= 1200 &&
+			filters.time <= 1600 &&
+			filters.number < 3 &&
+			filters.updates
+		) {
+			filters.message =
+				'<h1>It is a perfect time and situation to call mom!</h1><h2><p>Call her now!</h2>';
+		} else if (
+			filters.time >= 0600 &&
+			filters.time <= 0900 &&
+			filters.number < 5 &&
+			filters.updates
+		) {
+			filters.message =
+				'<h1>It is not the best time to call mom</h1><p><h2>See if it can wait.</h2>';
+		} else {
+			filters.message =
+				'<h1>Do not call your mom, its not a good situation.</h1><p><h2>Try again another day.</h2>';
+		}
+
+		const dailySummary = document.createElement('h3');
+		dailySummary.innerHTML = `${filters.day} ${filters.time} ${filters.number} ${filters.updates}`;
+		document.querySelector('#output').appendChild(dailySummary);
+
+		//daily.push(filters);
+		daily.splice(daily.length, 0, filters);
+		console.log(filters);
+		updateRender(daily[daily.length - 1]);
 	});
 
-const updateRender = function (filters) {
-	//daily.splice(daily.length + 1, 0, filters);
-	loopOverDaily(renderOutput(filters));
-	console.log(daily);
-	//clearAll(filters);
-	let outputMessage = document.createElement('p');
-	outputMessage.innerHTML = filters.message;
-	document.querySelector('#output').appendChild(outputMessage);
-	
-};
-
-// const clearAll = function () {
-// 	document.querySelector('#output').replaceChildren('');
-// };
-renderOutput(daily);
-//updateRender(filters);
-
-const summaryTitle = document.createElement('h1');
-summaryTitle.textContent = 'Should You Call Your Mom?';
-document.querySelector('#header').appendChild(summaryTitle);
 
