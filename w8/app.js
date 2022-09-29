f76c6822e69be8e08e243b0c301fece46348f2ee
bmcgauley <brian@mcgauley.com>
2022-09-29 12:00:44 -0700

    w8 test_02

--- w8/app.js
+++ w8/app.js
@@ -1,10 +1,12 @@
-const daily = [{
-	day: 'monday',
-	time: 1400,
-	number: 5,
-	updates: true,
-	message: '',
-}]
+const daily = [
+	{
+		day: 'monday',
+		time: 1400,
+		number: 5,
+		updates: true,
+		message: '',
+	},
+];
 
 const filters = {
 	day: 'monday',
@@ -27,11 +29,19 @@ const renderOutput = function (filters) {
 			'<h1>It is not a good time or situation to call mom</h1><p>Try again tomorrow</h2>';
 	}
 	
-	
 };
-const processDaily = function (daily) {
-	
-}
+const processDaily = function (obj) {
+	daily.forEach(function () {
+		renderOutput(obj);
+	});
+};
+const loopOverDaily = function () {
+	daily.forEach(function (obj) {
+		processDaily(obj);
+	});
+};
+
+loopOverDaily(renderOutput(filters));
 
 document.querySelector('#day-input').addEventListener('input', function (e) {
 	console.log(e.target.value);
@@ -60,24 +70,23 @@ document.querySelector('#updates').addEventListener('change', function (e) {
 document
 	.querySelector('#submit-button')
 	.addEventListener('click', function (e) {
-		
 		e.target.textContent = 'Submitted!';
 		renderOutput(filters);
-		
-		
 	});
-	
+
 document
 	.querySelector('#user-input-form')
 	.addEventListener('submit', function (e) {
 		e.preventDefault();
+		daily.push(filters);
 		console.log(filters);
-		
+
 		updateRender(filters);
 	});
 
 const updateRender = function (filters) {
-	daily.splice(daily.length+1, 0, filters);
+	//daily.splice(daily.length + 1, 0, filters);
+	loopOverDaily(renderOutput(filters));
 	console.log(daily);
 	//clearAll(filters);
 	let outputMessage = document.createElement('p');
@@ -89,9 +98,10 @@ const updateRender = function (filters) {
 // const clearAll = function () {
 // 	document.querySelector('#output').replaceChildren('');
 // };
-
+renderOutput(daily);
 //updateRender(filters);
 
 const summaryTitle = document.createElement('h1');
 summaryTitle.textContent = 'Should You Call Your Mom?';
 document.querySelector('#header').appendChild(summaryTitle);
+
