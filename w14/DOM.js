1db897e2216d6653e5c32dfcd8e74580a5f1b573
bmcgauley <brian@mcgauley.com>
2022-11-08 13:34:49 -0800

    W14

---
+++ w14/DOM.js
@@ -0,0 +1,33 @@
+'use strict';
+const orderDOM = () => {
+	// const orderDiv = document.querySelector('#order')
+	// const outputDiv = document.querySelector('#output')
+	// const outputTotalDiv = document.querySelector('#total');
+	const orderButton = document.querySelector('#order-button');
+
+	// const lineItem0 = document.querySelector('#product0')
+	// const lineItem1 = document.querySelector('#product1')
+	// const lineItem2 = document.querySelector('#product2')
+
+	orderButton.addEventListener('click', (e) => {
+		const order = new Order();
+		const orderProcess = order.randOrder();
+		let price0 = orderProcess.price0;
+		let price1 = orderProcess.price1;
+		let price2 = orderProcess.price2;
+		let item0 = orderProcess.item0;
+		let item1 = orderProcess.item1;
+		let item2 = orderProcess.item2;
+		let person = orderProcess.person;
+		let priceFinal = orderProcess.price;
+		runningArr.push(price0);
+		runningArr.push(price1);
+		runningArr.push(price2);
+		let priceTotal = parseInt(findSum(runningArr, runningArr.length));
+		const output = document.getElementById('output');
+		let total = document.getElementById('total');
+		output.innerHTML += `<u>${person}</u> placed an order for <li>${item0} - ${price0}<li>${item1} - ${price1}<li>${item2} - ${price2}<br /> with a cost of <b>${priceFinal}</b> <br />`;
+
+		total.innerHTML = `<h1>Total Order Cumulative Cost: <b><u>${priceTotal}</u></b></h1>`;
+	});
+};
---
+++ w14/app.js
@@ -0,0 +1,73 @@
+'use strict';
+
+orderDOM();
+class Order {
+	constructor() {
+		this.product = ['case','ram','motherboard','graphics-card','cpu','fan-kit','psu',];
+        this.quantity = [0,0,0,0,0,0,0,0]
+		this.person = ['Beth', 'John', 'Timmy', 'Brian', 'Alex', 'Scott', 'Daniel'];
+		this.price = [89, 150, 120, 700, 325, 60, 110];
+	}
+	randOrder() {
+        const randIndexPerson = Math.floor(Math.random() * this.person.length);
+		const productIndex0 = document.querySelector('#product0').value;
+        const index0 = this.product.indexOf(productIndex0)
+        const productIndex1 = document.querySelector('#product1').value;
+        const index1 = this.product.indexOf(productIndex1)
+        const productIndex2 = document.querySelector('#product2').value;
+        const index2 = this.product.indexOf(productIndex2)
+		const priceIndex0 = this.price[index0];
+        const priceIndex1 = this.price[index1];
+        const priceIndex2 = this.price[index2];
+        const productQuant0 = document.querySelector('#quant0').value;
+        const productQuant1 = document.querySelector('#quant1').value;
+        const productQuant2 = document.querySelector('#quant2').value;
+		const person = this.person[randIndexPerson];
+		const item0 = productIndex0;
+        const item1 = productIndex1;
+        const item2 = productIndex2;
+		const price0 = priceIndex0 * productQuant0;
+        const price1 = priceIndex1 * productQuant1;
+        const price2 = priceIndex2 * productQuant2;
+        let priceFinal = price0 + price1 + price2;
+		let order = {
+			item0: item0,
+            item1: item1,
+            item2: item2,
+			price0: price0,
+            price1: price1,
+            price2: price2,
+            price: priceFinal,
+			person: person,
+		};
+        const output = document.getElementById('output');
+        if (productQuant0 === '-')  {
+            output.setAttribute('class', 'error')
+            output.textContent = 'Please place a minimum order greater or equal to "0" in each section above'
+          } else if (productQuant1 === '-') {
+            output.setAttribute('class', 'error')
+            output.textContent = 'Please place a minimum order greater or equal to "0" in each section above'
+          } else if (productQuant2 === '-') {
+            output.setAttribute('class', 'error')
+            output.textContent = 'Please place a minimum order greater or equal to "0" in each section above'
+          } else {
+            
+            if (output.textContent === 'Please place a minimum order greater or equal to "0" in each section above') {
+                output.textContent = '';
+            } else {
+                output.setAttribute('class', 'orderOut')
+                return order;
+            }
+            
+          }  
+	}
+}
+
+let runningArr = [];
+const findSum = (arr, size) => {
+	let sum = 0;
+	for (let i = 0; i < size; i++) {
+		sum += arr[i];
+	}
+	return sum;
+};
