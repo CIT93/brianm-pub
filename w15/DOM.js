154f417a83cd0a18cbd99d709e3e72531566c8b2
bmcgauley <brian@mcgauley.com>
2022-11-16 12:44:55 -0800

    w15 cleanup

--- w15/DOM.js
+++ w15/DOM.js
@@ -1,9 +1,9 @@
-'use strict'
+'use strict';
 const mainDOM = () => {
-    const mainDiv = document.createElement('div')
-    mainDiv.setAttribute('id', 'maindiv')
-    const dataP = document.createElement('p')
-    dataP.setAttribute('id', 'data')
-    document.body.appendChild(mainDiv)
-    mainDiv.appendChild(dataP)
-}
+	const mainDiv = document.createElement('div');
+	mainDiv.setAttribute('id', 'maindiv');
+	const dataP = document.createElement('p');
+	dataP.setAttribute('id', 'data');
+	document.body.appendChild(mainDiv);
+	mainDiv.appendChild(dataP);
+};
--- w15/requests.js
+++ w15/requests.js
@@ -1,16 +1,15 @@
-'use strict'
+'use strict';
 const getTodos = (callback) => {
-    const request = new XMLHttpRequest();
+	const request = new XMLHttpRequest();
 
-    request.addEventListener('readystatechange', (e) => {
-        if (e.target.readyState === 4 && e.target.status === 200) {
-            const data = JSON.parse(e.target.responseText);
-            callback(undefined, data)
-        } else if (e.target.readyState === 4) {
-            callback('A network error has occured.', undefined)
-        }
-    })
-    request.open('GET', 'https://jsonplaceholder.typicode.com/todos')
-    request.send()
-}
-
+	request.addEventListener('readystatechange', (e) => {
+		if (e.target.readyState === 4 && e.target.status === 200) {
+			const data = JSON.parse(e.target.responseText);
+			callback(undefined, data);
+		} else if (e.target.readyState === 4) {
+			callback('A network error has occured.', undefined);
+		}
+	});
+	request.open('GET', 'https://jsonplaceholder.typicode.com/todos');
+	request.send();
+};
