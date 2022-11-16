9ceda798bb94692605159c1e818b79658f57957c
bmcgauley <brian@mcgauley.com>
2022-11-16 13:33:33 -0800

    w15 cleanup

--- w15/requests.js
+++ w15/requests.js
@@ -1,16 +1,14 @@
 'use strict';
-
 const getTodos = (callback) => {
-    const request = new XMLHttpRequest();
-
-    request.addEventListener('readystatechange', (e) => {
-        if (e.target.readyState === 4 && e.target.status === 200) {
-            const data = JSON.parse(e.target.responseText);
-            callback(undefined, data);
-        } else if (e.target.readyState === 4) {
-            callback('A network error has occured.', undefined);
-        }
-    });
-    request.open('GET', 'https://jsonplaceholder.typicode.com/todos');
-    request.send();
+	const request = new XMLHttpRequest();
+	request.addEventListener('readystatechange', (e) => {
+		if (e.target.readyState === 4 && e.target.status === 200) {
+			const data = JSON.parse(e.target.responseText);
+			callback(undefined, data);
+		} else if (e.target.readyState === 4) {
+			callback('A network error has occured.', undefined);
+		}
+	});
+	request.open('GET', 'https://jsonplaceholder.typicode.com/todos');
+	request.send();
 };
