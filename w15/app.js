f6f2da06d64782e0587946281843978a2d2e68c0
bmcgauley <brian@mcgauley.com>
2022-11-16 13:10:19 -0800

    w15 cleanup

--- w15/app.js
+++ w15/app.js
@@ -9,9 +9,7 @@ getTodos((error, todos) => {
 		todos.forEach((todo) => {
 			const div = document.createElement('div');
 			div.setAttribute('class', 'notDone');
-			todo.completed
-				? div.setAttribute('class', 'done')
-				: div.setAttribute('class', 'notDone');
+			todo.completed ? div.setAttribute('class', 'done') : div.setAttribute('class', 'notDone');
 			div.innerHTML = `<h3>User: ${todo.userId}</h3> Title: ${todo.title} <br />Entry Index: ${todo.id}<br />Completed: ${todo.completed}`;
 			list.appendChild(div);
 		});
