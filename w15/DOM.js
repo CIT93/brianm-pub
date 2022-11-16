f7532156fe31bffbe417e84630392244db75531c
bmcgauley <brian@mcgauley.com>
2022-11-16 12:39:00 -0800

    w15

---
+++ w15/DOM.js
@@ -0,0 +1,9 @@
+'use strict'
+const mainDOM = () => {
+    const mainDiv = document.createElement('div')
+    mainDiv.setAttribute('id', 'maindiv')
+    const dataP = document.createElement('p')
+    dataP.setAttribute('id', 'data')
+    document.body.appendChild(mainDiv)
+    mainDiv.appendChild(dataP)
+}
