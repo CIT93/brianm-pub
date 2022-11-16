97fba4cdaed9ecd77c3626a8b7e3a74fac4e02c7
bmcgauley <brian@mcgauley.com>
2022-11-16 13:33:04 -0800

    w15 wrapped getTodos in a button

--- w15/DOM.js
+++ w15/DOM.js
@@ -4,6 +4,10 @@ const mainDOM = () => {
 	mainDiv.setAttribute('id', 'maindiv');
 	const dataP = document.createElement('p');
 	dataP.setAttribute('id', 'data');
+    const button = document.createElement('button');
+    button.setAttribute('id', 'gettodos');
+    button.textContent = 'Get Todos List'
 	document.body.appendChild(mainDiv);
 	mainDiv.appendChild(dataP);
+    mainDiv.appendChild(button);
 };
--- w15/app.js
+++ w15/app.js
@@ -1,17 +1,19 @@
 'use strict';
 mainDOM();
 const mainDiv = document.querySelector('#maindiv');
-getTodos((error, todos) => {
-	if (error) {
-		new Error('Error');
-	} else {
-		const list = document.querySelector('#maindiv');
-		todos.forEach((todo) => {
-			const div = document.createElement('div');
-			div.setAttribute('class', 'notDone');
-			todo.completed ? div.setAttribute('class', 'done') : div.setAttribute('class', 'notDone');
-			div.innerHTML = `<h3>User: ${todo.userId}</h3> Title: ${todo.title} <br />Entry Index: ${todo.id}<br />Completed: ${todo.completed}`;
-			list.appendChild(div);
-		});
-	}
-});
+document.getElementById('gettodos').addEventListener('click', (e) => {
+    getTodos((error, todos) => {
+        if (error) {
+            new Error('Error');
+        } else {
+            const list = document.querySelector('#maindiv');
+            todos.forEach((todo) => {
+                const div = document.createElement('div');
+                div.setAttribute('class', 'notDone');
+                todo.completed ? div.setAttribute('class', 'done') : div.setAttribute('class', 'notDone');
+                div.innerHTML = `<h3>User: ${todo.userId}</h3> Title: ${todo.title} <br />Entry Index: ${todo.id}<br />Completed: ${todo.completed}`;
+                list.appendChild(div);
+            });
+        }
+    });
+})
