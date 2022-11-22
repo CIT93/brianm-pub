b8d0231bc776906562c52122a6f51f56b5dcbb92
bmcgauley <brian@mcgauley.com>
2022-11-21 16:41:18 -0800

    w16 draft still

--- w16/app.js
+++ w16/app.js
@@ -1,21 +1,176 @@
 'use strict';
 mainDOM();
 const mainDiv = document.querySelector('#maindiv');
-document.getElementById('gettodos').addEventListener('click', (e) => {
+document.getElementById('gettodos').addEventListener('click', () => {
 	getTodos()
 		.then((todos) => {
-			const list = document.querySelector('#maindiv');
-			todos.forEach((todo) => {
-				const div = document.createElement('div');
-				div.setAttribute('class', 'notDone');
-				todo.completed
-					? div.setAttribute('class', 'done')
-					: div.setAttribute('class', 'notDone');
-				div.innerHTML = `<h3>User: ${todo.userId}</h3> Title: ${todo.title} <br />Entry Index: ${todo.id}<br />Completed: ${todo.completed}`;
-				list.appendChild(div);
-			});
+			getStructure(todos);
 		})
+        .then((todos) => {
+            showAll(todos);
+        })
 		.catch((err) => {
 			console.log(`Error: ${err}`);
 		});
 });
+document.getElementById('completed').addEventListener('change', () => {
+    if (document.getElementById('completed').checked) {
+        document.getElementById('incomplete').checked = false
+    }
+	if (document.getElementById('completed').checked && document.getElementById('incomplete').checked === false) {
+		getTodos()
+			.then((todos) => {
+				getStructure(todos);
+			}).then((todos) => {
+                showAll(todos);
+                showCompleted(todos);
+            })
+			.catch((err) => {
+				console.log(`Error: ${err}`);
+			});
+	} else if (document.getElementById('completed').checked && document.getElementById('incomplete').checked) {
+		getTodos()
+		.then((todos) => {
+			getStructure(todos);
+		})
+        .then((todos) => {
+            showCompleted(todos);
+            showIncomplete(todos);
+        })
+		.catch((err) => {
+			console.log(`Error: ${err}`);
+		});
+	} else {
+        getTodos()
+		.then((todos) => {
+			getStructure(todos);
+		})
+        .then((todos) => {
+            showAll(todos);
+        })
+		.catch((err) => {
+			console.log(`Error: ${err}`);
+		});
+    }
+    
+});
+document.getElementById('incomplete').addEventListener('change', () => {
+    if (document.getElementById('incomplete').checked) {
+        document.getElementById('completed').checked = false
+    }
+	if (document.getElementById('incomplete').checked && document.getElementById('completed').checked === false) {
+		getTodos()
+			.then((todos) => {
+				getStructure(todos);
+			}).then((todos) => {
+                showAll(todos)
+                showIncomplete(todos)
+            })
+			.catch((err) => {
+				console.log(`Error: ${err}`);
+			});
+	} else if (document.getElementById('completed').checked && document.getElementById('incomplete').checked) {
+		getTodos()
+		.then((todos) => {
+			getStructure(todos);
+		})
+        .then((todos) => {
+            showCompleted(todos);
+            showIncomplete(todos);
+        })
+		.catch((err) => {
+			console.log(`Error: ${err}`);
+		});
+    } else {
+        getTodos()
+		.then((todos) => {
+			getStructure(todos);
+		})
+        .then((todos) => {
+            showAll(todos);
+        })
+		.catch((err) => {
+			console.log(`Error: ${err}`);
+		});
+    }
+});
+const showCompleted = () => {
+	const completed = document.querySelectorAll('div');
+	completed.forEach((todo) => {
+		if (todo.textContent.includes('true')) {
+			todo.setAttribute('class', 'done');
+		} else {
+			//todo.style.display = 'none';
+		}
+	});
+};
+const showIncomplete = () => {
+	const incomplete = document.querySelectorAll('div');
+	incomplete.forEach((todo) => {
+		if (todo.textContent.includes('false')) {
+			todo.setAttribute('class', 'incomplete');
+		} else {
+			//todo.style.display = 'none';
+		}
+	});
+};
+const showAll = () => {
+    const all = document.querySelectorAll('div');
+    all.forEach((todo) => {
+        if (todo.textContent.includes('true') || todo.textContent.includes('false')) {
+            todo.setAttribute('class', 'all');
+        } else {
+            //todo.style.display = 'none';
+        }
+    })
+}
+// document.getElementById('user-button').addEventListener('click', () => {
+//     getTodos()
+//     .then((todos) => {
+//         getStructure(todos);
+//     })
+//     .then((todos) => {
+//         showAll(todos);
+//     })
+//     .then((todos) => {
+//         userFilter(todos)
+//     })
+//     .catch((err) => {
+//         console.log(`Error: ${err}`);
+//     });
+    
+// })
+// const userFilter = () => {
+//     getTodos()
+//     .then((todos) => {
+//         getStructure(todos)
+//     })
+//     .then((todos) => {
+//         let user = document.querySelector('user').value
+//         user.forEach((todo) => {
+//             if (todo.userId === 1) {
+//                 todo.setAttribute('class', 'user');
+//             } else if (todo.userId === 2) {
+//                 todo.setAttribute('class', 'user');
+//             } else if (todo.userId === 3) {
+//                 todo.setAttribute('class', 'user');
+//             } else if (todo.userId === 4) {
+//                 todo.setAttribute('class', 'user');
+//             } else if (todo.userId === 5) {
+//                 todo.setAttribute('class', 'user');
+//             } else if (todo.userId === 6) {
+//                 todo.setAttribute('class', 'user');
+//             } else if (todo.userId === 7) {
+//                 todo.setAttribute('class', 'user');
+//             } else if (todo.userId === 8) {
+//                 todo.setAttribute('class', 'user');
+//             } else if (todo.userId === 9) {
+//                 todo.setAttribute('class', 'user');
+//             } else if (todo.userId === 10) {
+//                 todo.setAttribute('class', 'user');
+//             } else {
+//                 todo.style.display = 'none'
+//             }
+//     })
+//     })
+// }
--- w16/requests.js
+++ w16/requests.js
@@ -8,4 +8,13 @@ const getTodos = async () => {
 		throw new Error('Unable to get data');
 	}
 };
-//
+//structure todos
+const getStructure = (todos) => {
+	const list = document.querySelector('#maindiv');
+	todos.forEach((todo) => {
+		const div = document.createElement('div');
+		div.setAttribute('class', 'all');
+		div.innerHTML = `<h3>User: ${todo.userId}</h3> Title: ${todo.title} <br />Entry Index: ${todo.id}<br />Completed: ${todo.completed}`;
+		list.appendChild(div);
+	});
+};
